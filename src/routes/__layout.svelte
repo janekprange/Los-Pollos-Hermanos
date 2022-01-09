a41b32d2f3778b7aa2b013035856fb217263f35e
Janek Prange <janek.prange@stud.uni-hannover.de>
2022-01-09 22:19:35 +0100

    first page finished

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<div class="background">
+	<slot />
+</div>
+
+<style>
+	.background {
+		background-color: #4c0000;
+		height: 100vh;
+		display: flex;
+		border: 1px solid red;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,44 @@
-<div>
-	<h1>Willkommen bei Los Hermanos Pollos</h1>
+<div class="container">
+	<img src="/logo.jpg" alt="Logo" />
+	<h1>Willkommen bei Los Pollos Hermanos</h1>
 
-	<a href="reservieren">Reservieren</a>
+	<div class="link">
+		<a href="reservieren">Reservieren</a>
+	</div>
 </div>
+
+<style>
+	.container {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: baseline;
+	}
+
+	img {
+		max-width: 300px;
+		margin: auto;
+	}
+
+	h1 {
+		flex-grow: 1;
+		text-align: center;
+		color: white;
+	}
+
+	.link {
+		flex-grow: 2;
+		text-align: center;
+	}
+	a {
+		background-color: #b8977e;
+		border-radius: 5px;
+		padding: 1em 3em 1em 3em;
+		color: #ffffff;
+		font-family: Berkshire Swash;
+		font-size: 24px;
+		line-height: 62px;
+		width: 264px;
+		text-align: center;
+	}
+</style>

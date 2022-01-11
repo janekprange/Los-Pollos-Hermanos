99eedbc19d08226ebfd7716b0fb29f0cd5e4f1a9
Janek Prange <janek.prange@stud.uni-hannover.de>
2022-01-11 12:00:38 +0100

    Tischauswahl hinzugefügt

--- src/routes/reservieren/platzwahl.svelte
+++ src/routes/reservieren/platzwahl.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import Datepicker from '$lib/components/Datepicker.svelte';
+	import Multiselect from '$lib/components/Multiselect.svelte';
 	import Timepicker from '$lib/components/Timepicker.svelte';
 
 	import { fade } from 'svelte/transition';
 
 	let date: Date;
 	let dauer: number; // Dauer in Millisekunden
-
 	$: dateIsToday = date && Date.now() + 1000 * 60 * 60 * 24 > date.valueOf();
+	let table: number[] = [];
+
+	const tablenumbers = Array.from({ length: 39 }, (v, k) => k + 1);
 </script>
 
 <div class="container">
@@ -15,6 +18,13 @@
 		<h1>Platzwahl</h1>
 		<Datepicker bind:date />
 		<Timepicker bind:dauer />
+		{#if date && dauer}
+			<Multiselect id="table" bind:value={table} placeholder="Bitte den Tisch wählen">
+				{#each tablenumbers as table}
+					<option value={table}>{table}</option>
+				{/each}
+			</Multiselect>
+		{/if}
 	</div>
 	{#if date && dauer}
 		<!-- {#if dateIsToday}

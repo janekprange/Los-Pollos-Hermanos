d62b8e90c51f86e1b85e80eb7e4c6730de0d2788
Janek Prange <janek.prange@stud.uni-hannover.de>
2022-01-20 20:47:12 +0100

    fix sessionstore

--- src/lib/stores/reservierung.js
+++ src/lib/stores/reservierung.js
@@ -1,22 +1,22 @@
 import { writable } from "svelte/store"
 import { browser } from "$app/env"
 
-export const resDatum = writable(browser && new Date(Date.parse(localStorage.getItem("datum"))))
+export const resDatum = writable(browser && new Date(Date.parse(sessionStorage.getItem("datum"))))
 resDatum.subscribe((val) => {
   if (browser) return (sessionStorage.datum = val)
 })
 
-export const resDauer = writable(browser && Number(localStorage.getItem("dauer")))
+export const resDauer = writable(browser && Number(sessionStorage.getItem("dauer")))
 resDauer.subscribe((val) => {
   if (browser) return (sessionStorage.dauer = val)
 })
 
-export const resTisch = writable(browser && JSON.parse(`[${localStorage.getItem("tisch")}]`).map((val) => val ? val.toString() : null))
+export const resTisch = writable(browser && JSON.parse(`[${sessionStorage.getItem("tisch")}]`).map((val) => val ? val.toString() : null))
 resTisch.subscribe((val) => {
   if (browser) return (sessionStorage.tisch = val)
 })
 
-export const user = writable(browser && JSON.parse(localStorage.getItem("user")))
+export const user = writable(browser && JSON.parse(sessionStorage.getItem("user")))
 user.subscribe((val) => {
   if (browser) return (sessionStorage.user = val)
 })
